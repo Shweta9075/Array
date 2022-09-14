e81528f4f02d57701ec014f5b95f2cb7a7b854d8
shweta <shwetabirajdar5872gmail.com>
2022-09-14 22:30:14 +0530

    Added: elements in descending order

--- src/com/bridgelabz/DescendingOrder.java
+++ src/com/bridgelabz/DescendingOrder.java
@@ -1,7 +1,8 @@
 package com.bridgelabz;
 
-public class AscendingOrder {
+public class DescendingOrder {
     public static void main(String[] args) {
+
         int[] arr = new int[] {5,2,8,7,1};
         int temp = 0;
 
@@ -10,25 +11,19 @@ public class AscendingOrder {
             System.out.print(arr[i] + " ");
         }
 
-        //sort the array in ascending order
-
         for(int i=0; i<arr.length; i++) {
             for(int j=i+1; j<arr.length; j++) {
-                if(arr[i]>arr[j]) {
-                    temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
+                if(arr[i] < arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
         System.out.println();
-
-        //Display element of array sorting
-        System.out.println("Element of array sorted in ascending order : ");
+        System.out.println("Element of array sorted in Descending order :  ");
         for(int i=0; i<arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
-
     }
-
 }
