21f08cb147c000a1a7f36f27e083838b799d6d61
shweta <shwetabirajdar5872gmail.com>
2022-09-14 21:44:37 +0530

    Added:Finding elements in array

---
+++ src/com/bridgelabz/ElementsOfArray.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class ElementsOfArray {
+    public static void main(String[] args) {
+
+        int[] arr = {2,4,6,8,10};
+
+        System.out.println("Enements Of an array : ");
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
